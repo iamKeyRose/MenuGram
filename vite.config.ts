0b9eb6216d7de924ac876cbd2d7b981e3a991222
iamKeyRose <iamkeyrose.tech@gmail.com>
2026-04-14 18:35:24 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: './', // <--- Change this from '/' to './'
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    // This ensures your build doesn't fail on small type warnings
+    minify: 'esbuild',
+    reportCompressedSize: false,
   }
 });
