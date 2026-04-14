7c7ee9b9b4e1ca6def659d88aed1225f56572310
iamKeyRose <iamkeyrose.tech@gmail.com>
2026-04-14 16:39:05 +0400

    Update useTelegram.ts

--- Telegram SDK script/src/hooks/useTelegram.ts
+++ Telegram SDK script/src/hooks/useTelegram.ts
@@ -1,1 +1,14 @@
+export const useTelegram = () => {
+  const tg = (window as any).Telegram?.WebApp;
+
+  return {
+    tg,
+    user: tg?.initDataUnsafe?.user,
+    queryId: tg?.initDataUnsafe?.query_id,
+    expand: () => tg?.expand(),
+    close: () => tg?.close(),
+    MainButton: tg?.MainButton,
+    ready: () => tg?.ready(),
+  };
+};
 
