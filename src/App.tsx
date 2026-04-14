58a503b51cfb748bfec755cee4decab1edb90e03
iamKeyRose <iamkeyrose.tech@gmail.com>
2026-04-14 20:02:06 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import { Home } from './pages/Home';
-//import { Orders } from './pages/Orders'; // You'll create this next
-//import { Profile } from './pages/Profile'; // You'll create this next
+import { Home } from './pages/home';
+//import { Orders } from './pages/orders'; // You'll create this next
+//import { Profile } from './pages/profile'; // You'll create this next
 import { BottomNav } from './components/BottomNav';
 import { useTelegram } from './hooks/useTelegram';
 
